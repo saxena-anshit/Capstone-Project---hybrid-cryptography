52f48013376096aa21f97680f3443b3e1a492115
saxena-anshit <saxena.anshit@gmail.com>
2019-03-28 01:48:06 +0530

    test files fro Hash/ One time pad/ and RSA added

---
+++ Utilities/Test_Case_Hash_Algorithm.py
@@ -0,0 +1,18 @@
+from Hash_Algorithm import hash_function
+
+import unittest
+import hashlib
+
+
+class TestStringMethods(unittest.TestCase): 
+    
+    def setUp(self): 
+        pass
+
+
+    def test_modular_hash_function(self):		 
+        self.assertEqual(hash_function("hello"), hashlib.sha256("hello".encode('utf-8')).hexdigest())
+
+
+if __name__ == '__main__': 
+    unittest.main() 
---
+++ Utilities/Test_Case_One_Time_Pad.py
@@ -0,0 +1,16 @@
+from One_Time_Pad import one_time_pad_password
+
+import unittest
+import math
+
+class TestStringMethods(unittest.TestCase): 
+    
+    def setUp(self): 
+        pass
+
+    def test_one_time_pad_password(self): 
+        self.assertEqual(len(one_time_pad_password()), 256)
+
+
+if __name__ == '__main__': 
+    unittest.main() 

04171cbb618fdc11566917346a37d1484d5220f0
Abdemanaaf <abdemanaafzghadiali@gmail.com>
2019-03-28 12:55:51 +0530

    changes in encrypt file changes in decrypt file
    test cases added

--- Utilities/Hash_Algorithm.py
+++ Utilities/Hash_Algorithm.py
@@ -1,7 +1,9 @@
 import hashlib
 
+
 def hash_function(plain_text):
     return hashlib.sha256(plain_text.encode('utf-8')).hexdigest()
+
 
 if __name__ == '__main__':
     plain_text = input("Enter a message to encrypt: ")
--- Utilities/One_Time_Pad.py
+++ Utilities/One_Time_Pad.py
@@ -1,7 +1,8 @@
 from Crypto.Util import number
 
+
 def one_time_pad_password():
-    return (bin(number.getPrime(16))[2:])
+    return bin(number.getPrime(16))[2:]
 
 if __name__ == '__main__':
     password = one_time_pad_password()

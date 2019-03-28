25f4523d62f54e5d018b724c754b38f9f20e7c54
Abdemanaaf <abdemanaafzghadiali@gmail.com>
2019-03-28 11:31:45 +0530

    one time pad algo added

--- Test_Case_Hash_Algorithm.py
+++ Test_Case_Hash_Algorithm.py
@@ -1,4 +1,4 @@
-from Hash_Algorithm import hash_function
+from Utilities.Hash_Algorithm import hash_function
 
 import unittest
 import hashlib
@@ -10,7 +10,7 @@ class TestStringMethods(unittest.TestCase):
         pass
 
 
-    def test_modular_hash_function(self):		 
+    def test_modular_hash_function(self):
         self.assertEqual(hash_function("hello"), hashlib.sha256("hello".encode('utf-8')).hexdigest())
 
 

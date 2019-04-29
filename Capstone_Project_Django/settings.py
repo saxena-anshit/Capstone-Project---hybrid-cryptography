c9e5222c401bf8e0ae2c995b56620fe60a0f28fa
Abdemanaaf <abdemanaafzghadiali@gmail.com>
2019-04-30 00:53:31 +0530

    Coverage Reports

--- Capstone_Project_Django/settings.py
+++ Capstone_Project_Django/settings.py
@@ -22,7 +22,8 @@ INSTALLED_APPS = [
 ]
 
 JENKINS_TASKS = (
-    'django_jenkins.tasks.run_pylint',
+    'django_jenkins.tasks.run_pep8',
+    'django_jenkins.tasks.run_pyflakes',
 )
 
 MIDDLEWARE = [

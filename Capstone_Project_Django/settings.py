5c6a0f8416d3765b297101f1222111b91bd37c8c
Abdemanaaf <abdemanaafzghadiali@gmail.com>
2019-04-29 19:48:47 +0530

    Django-Jenkins

--- Capstone_Project_Django/settings.py
+++ Capstone_Project_Django/settings.py
@@ -18,7 +18,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_jenkins',
 ]
+
+JENKINS_TASKS = (
+        'django_jenkins.tasks.with_coverage',
+        'django_jenkins.tasks.django_tests',
+        'django_jenkins.tasks.run_pep8',
+        'django_jenkins.tasks.run_pyflakes',
+    )
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

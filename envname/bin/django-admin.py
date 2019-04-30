b7173eb498e8a967b3aeb3a19663d17b4dca55b9
saxena-anshit <saxena.anshit@gmail.com>
2019-04-30 07:31:08 +0530

    hi added

---
+++ envname/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ansh/test/Capstone-Project---hybrid-cryptography/envname/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
